dc94cc90e765c710071d40e2e9e14b518e19fa8f
Rongzhi-Liang <140492815+Rongzhi-Liang@users.noreply.github.com>
2026-09-08 21:55:56 +0800

    Delete site directory

--- site/src/env.d.ts
+++
@@ -1,2 +0,0 @@
-interface ImportMetaEnv { readonly VITE_MAPBOX_TOKEN: string; readonly VITE_MAPBOX_SOURCE_MODE: string; readonly BASE_URL: string }
-interface ImportMeta { readonly env: ImportMetaEnv }
